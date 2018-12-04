aebff216824cc537c9044535e915ed2e31ee450b
h0wiechan <howiechanhy@outlook.com>
2018-12-03 22:34:15 -0500

    Allowed core function to accept Document API

--- memepool/app.js
+++ memepool/app.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const documentEl = $$(document);
     const bgBars = $$(".bg-bars").children();
     $$(function() {
         bgBars.each(bar => {
@@ -14,16 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
             // }, 0);
         });
     });
-    memepool = new MemePool();
+    const options = {
+        documentEl: documentEl,
+    };
+    memepool = new MemePool(options);
     memepool.render();
 });
 
 class MemePool {
-    constructor() {
-        this.header = new Header();
-        this.uploadForm = new UploadForm();
-        this.bar = new Bar({uploadForm: this.uploadForm.form});
-        this.memesContainer = new MemesContainer();
+    constructor(options) {
+        this.header = new Header(options);
+        this.uploadForm = new UploadForm(options);
+        this.bar = new Bar(Object.assign(options, {uploadForm: this.uploadForm.form}));
+        this.memesContainer = new MemesContainer(options);
     }
 
     render() {
@@ -33,8 +37,54 @@ class MemePool {
     }
 }
 
-class Header {
-    constructor() {
+class Komponent {
+    constructor(options) {
+        this.documentEl = options.documentEl;
+    }
+    renderChildren(el, klass, t, cb = null, devault = true) {
+        for (let i = 0; i < el.children().length; i++) {
+            const child = el.children()[i];
+            if (devault) setTimeout(() => $(child).removeClass(klass), t);
+            if (cb) cb(child, t);
+        }
+    }
+
+    renderChildrenInOrder(el, klass, t, cb = null, devault = true) {
+        const dt = t / el.children().length;
+        for (let i = 0; i < el.children().length; i++) {
+            const childEl = el.children()[i];
+            const child = $(childEl);
+            if (devault) setTimeout(() => child.removeClass(klass), t);
+            // if (cb) cb(child, t / child.children().length);
+            if (cb) cb(child, t);
+            t += dt;
+        }
+    }
+    // for one-level destructuring
+    // (e.g. <li><i class"fa fas=something></i> Something</li>)
+
+    renderChildrenInOrderEvenly(el, klass, t, cb = null, devault = true) {
+        const dt = t / el.children().length;
+        for (let i = 0; i < el.children().length; i++) {
+            const child = el.children()[i];
+            const element = $(child);
+            // if ($(child).hasClass("hidden")) {
+            if (element.children().length > 0) {
+                if (devault) () => element.removeClass(klass);
+                this.renderChildrenInOrderEvenly(element, t, cb, devault);
+                t += dt / element.children().length;
+            } else {
+                if (devault) setTimeout(() => element.removeClass(klass), t);
+                if (cb) cb(element);
+                t += dt;
+            }
+        }
+    }
+}
+
+class Header extends Komponent {
+    constructor(options) {
+        super(options);
         this.header = $$("header.hidden");
     }
 
@@ -43,8 +93,9 @@ class Header {
     }
 }
 
-class Bar {
+class Bar extends Komponent {
     constructor(options) {
+        super(options);
         this.bar = $$(".bar.hidden")
         this.uploadButton = $$(".add-button");
         this.uploadForm = options.uploadForm;
@@ -82,8 +133,9 @@ class Bar {
     }
 }
 
-class UploadForm {
-    constructor() {
+class UploadForm extends Komponent {
+    constructor(options) {
+        super(options);
         this.form = $$("form.hidden");
     }
 
@@ -92,8 +144,9 @@ class UploadForm {
     }
 }
 
-class MemesContainer {
-    constructor() {
+class MemesContainer extends Komponent {
+    constructor(options) {
+        super(options);
         this.memesContainer = $$(".memes-container.hidden");
     }
 
